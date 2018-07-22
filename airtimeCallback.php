bdda2d9b4b76259a01503d658348110d708ab06e
brianokinyi <brianokinyi.bo@gmail.com>
2018-07-23 00:49:21 +0300

    Airtime call back

---
+++ airtimeCallback.php
@@ -0,0 +1,5 @@
+<?php
+    $status = $_POST['status']; //This is either Success or Failed
+    $requestId = $_POST['requestId']; //This is the request id received from the API when the airtime was sent
+    //Update your records
+?>
